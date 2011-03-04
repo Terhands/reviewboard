510dd7503fd3e47ab5de602398ed18a81dbabea2
Terhands <tch562@mail.usask.ca>
2011-03-04 10:43:06 -0600

    removed extra whitespace

--- reviewboard/template_loader.py
+++ reviewboard/template_loader.py
@@ -6,7 +6,7 @@ class ExtensionLoader(app_directories.Loader):
     is_usable = True
 
     def load_template(self, template_name, template_dirs=None):
-        """Template loader that loads templates with the _extended suffix or 
+        """Template loader that loads templates with the _extended suffix or
             reviewboard/ prefix"""
 
         template_dirs = getattr(settings, "TEMPLATE_DIRS", [])
@@ -17,7 +17,7 @@ class ExtensionLoader(app_directories.Loader):
         if template_name.startswith("reviewboard/"):
             template_name.replace("reviewboard/")
         else:
-            template_name.replace(".html", "_extended.html")    
+            template_name.replace(".html", "_extended.html")
 
         source, origin = self.load_template_source(template_name, template_dirs)
         template = Template(source)
